9857b564784a13d0914c81817288775b2b8e005e
ToshifumiIde <toshifumiide198765@gmail.com>
2020-12-25 21:23:00 +0900

    create schedules redux

---
+++ src/redux/addSchedule/actions.js
@@ -0,0 +1,19 @@
+export const ADD_SCHEDULE_SET_VALUE = "ADD_SCHEDULE_SET_VALUE";
+export const ADD_SCHEDULE_OPEN_DIALOG = "ADD_SCHEDULE_OPEN_DIALOG";
+export const ADD_SCHEDULE_CLOSE_DIALOG = "ADD_SCHEDULE_CLOSE_DIALOG";
+
+//formの値を更新するaction
+export const addScheduleDialog = payload =>({
+  type:ADD_SCHEDULE_SET_VALUE,
+  payload
+});
+//dialogを開くaction
+export const addScheduleOpenDialog = payload =>({
+  type:ADD_SCHEDULE_OPEN_DIALOG,
+  payload
+});
+////dialogを閉じるaction
+export const addScheduleCloseDialog = payload =>({
+  type:ADD_SCHEDULE_CLOSE_DIALOG,
+  payload
+});
---
+++ src/redux/addSchedule/reducer.js
@@ -0,0 +1,36 @@
+import {
+  ADD_SCHEDULE_SET_VALUE,
+  ADD_SCHEDULE_OPEN_DIALOG,
+  ADD_SCHEDULE_CLOSE_DIALOG,
+} from "./actions";
+
+//初期の予定（空の予定を設定）
+const init = {
+  form:{
+    title:"",
+    description:"",
+    date:null,
+    location:""
+  },
+  isDialogOpen:false,
+};
+
+const addScheduleReducer = ( state = init , action ) => {
+  const { type , payload } = action;
+
+  switch(type){
+    case ADD_SCHEDULE_SET_VALUE:
+      return {...state , form:{...state.form, ...payload}};
+      //過去に入力された予定を分割代入（展開）し、新しいformを追加する。
+      //また新しいformの中でも、現在のformに対して「新しいデータ」を展開して追加する
+    case ADD_SCHEDULE_OPEN_DIALOG:
+      return {...state , isDialogOpen:true };
+      //過去に入力された予定を分割代入（展開）し、
+    case ADD_SCHEDULE_CLOSE_DIALOG:
+      return init;
+    default :
+      return state;
+  }
+};
+
+export default addScheduleReducer;
